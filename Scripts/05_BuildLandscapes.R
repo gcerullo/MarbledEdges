314750078babadf71d1f79e732a300efc33e5b97
gcerullo <gcerullo95@gmail.com>
2024-11-28 13:51:49 -0800

    composition matched landscapes

--- Scripts/05_BuildLandscapes.R
+++ Scripts/05_BuildLandscapes.R
@@ -300,17 +300,17 @@ nb_cols <- 3333 # Number of columns
 
 cls_a <- flsgen_create_class_targets(
   "Plantation",
-  NP = c(1, 1000),  #number of patches
-  AREA = c(1, 1), #patch area min cells, max cells 
-  CA = c(1000, 5000) #total class area min and max 
-
+  NP = c(1, 100),  #number of patches
+  AREA = c(100, 100), #patch area min cells, max cells 
+  CA = c(1, 4000), #total class area min and max 
+  PLAND = c(77*0.6, 77*0.6)
  # MESH = c(225, 225) #effective mesh size
 )
 cls_b <- flsgen_create_class_targets(
   "Forest",
-  NP = c(1, 1),
-  AREA = c(200, 4000),
-  PLAND = c(30, 30) #proportion of landscapes
+  NP = c(1, 100),
+  AREA = c(1, 100),
+  PLAND = c(26.9*0.6, 26.9*0.6) #proportion of landscapes
 )
 
 200*200
@@ -323,3 +323,46 @@ ls_targets <- flsgen_create_landscape_targets(
 structure <- flsgen_structure(ls_targets)
 landscape <- flsgen_generate(structure_str = structure)
 plot(landscape)
+
+
+cls_1 <- flsgen_create_class_targets("forest",
+                                     NP=c(1, 10),
+                                     AREA=c(10000, 10000))
+cls_2 <- flsgen_create_class_targets("plantation",
+                                     AREA=c(1000,10000), 
+                                     NP = c(1, 5))
+
+#instead create a series of landscapes where we modify forest patch-size
+ls_targets <- flsgen_create_landscape_targets(200, 200, list(cls_1, cls_2))
+
+target_series <- flsgen_create_target_series(ls_targets,
+                                             class_name="forest",
+                                             target_key="NP", sequence=seq(1, 10, by=1))
+
+
+# --- Generate a terrain raster  ---
+terrain <- flsgen_terrain(width = 200, height = 200, roughness = 0.5)
+plot(terrain)
+# --- Loop through the target series ---
+for (i in 1:length(target_series)) { 
+  
+  # Extract current targets
+  targets <- target_series[[i]] 
+  
+  # Generate landscape structure 
+  structure <- flsgen_structure(targets_str = targets)
+  
+  # Generate landscape raster 
+  landscape_raster <- flsgen_generate(
+    structure_str = structure,
+    terrain_file = terrain,  # Use the generated terrain
+    min_distance = 2, 
+    terrain_dependency = 0.8  # Example value, adjust as needed
+  )
+  
+  # Plot the raster
+  plot(landscape_raster)
+  title(main = paste("Forest NP:", targets$classes[[1]]$NP)) 
+  
+}
+
